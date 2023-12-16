a8f6b046be9880fb3c1be7f75667301978e64006
Joe Baumgartner <3257771+joeybaumgartner@users.noreply.github.com>
2023-12-16 10:02:34 -0600

    Refactor Player into separate file

---
+++ Player.py
@@ -0,0 +1,27 @@
+class Player:  #class Player constructor
+    def __init__(self, playernumber):
+        self.playernumber = playernumber    #Class Player gets a "playernumber" attribute
+        self.cardsinhand = []               #Class Player gets a "cardsinhand" attribute
+        self.playername = ""                #gives players a name attribute in the class
+        self.collected_clues = []           #where the information goes that the player collects
+        self.dialed_this_turn = False       #a flag for limiting 1 dial per turn
+        self.guessed_this_turn = False      #only one guess per turn flag
+        self.pvp_in_hand = []
+        self.pvp_this_turn = False
+
+    # Reset all turn-based flags
+    def end_turn(self):
+        self.dialed_this_turn = False
+        self.guessed_this_turn = False
+        self.pvp_this_turn = False
+
+    # Prints players' hand; both "Boy" card and PvP cards
+    def print_hand(self): 
+        print(f"{self.playername}'s current hand is")
+        for i in self.cardsinhand:
+            print(f"{i.name} - Phone#: {i.phonenum}")
+
+        print("PvP", end = " ")
+        for i in self.pvp_in_hand:
+            print(f"|{i.long_name}|", end = " ")
+        print(end = "\n")
--- main.py
+++ main.py
@@ -13,35 +13,7 @@ import click # Import click library for good screen clearing function
 colorama.init() #turns on windows shell fix
 
 from DreamEnums import PvpCardType
-
-class Player:  #class Player constructor
-    def __init__(self, playernumber):
-        self.playernumber = playernumber    #Class Player gets a "playernumber" attribute
-        self.cardsinhand = []               #Class Player gets a "cardsinhand" attribute
-        self.playername = ""                #gives players a name attribute in the class
-        self.collected_clues = []           #where the information goes that the player collects
-        self.dialed_this_turn = False       #a flag for limiting 1 dial per turn
-        self.guessed_this_turn = False      #only one guess per turn flag
-        self.pvp_in_hand = []
-        self.pvp_this_turn = False
-
-    # Reset all turn-based flags
-    def end_turn(self):
-        self.dialed_this_turn = False
-        self.guessed_this_turn = False
-        self.pvp_this_turn = False
-
-    # Prints players' hand; both "Boy" card and PvP cards
-    def print_hand(self): 
-        print(f"{self.playername}'s current hand is")
-        for i in self.cardsinhand:
-            print(f"{i.name} - Phone#: {i.phonenum}")
-            short_delay()
-
-        print("PvP", end = " ")
-        for i in self.pvp_in_hand:
-            print(f"|{i.long_name}|", end = " ")
-        print(end = "\n")
+from Player import Player
 
 player1 = Player(1)
 player2 = Player(2)
@@ -696,6 +668,7 @@ def game_loop():
     while not game_ended:
         whos_turn(current_player)
         current_player.print_hand()
+        short_delay()
         check_decks()
         print (white_out("Commands: ('dial') - ('notepad') - ('pvp') - ('solve') - ('redial') - ('end')"),"\n")
         choice = input().lower()
